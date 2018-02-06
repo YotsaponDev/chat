f4562741c40cacb6e234da79f41d895e3ad7d501
yotsapon6 <boss.yotsapon@gmail.com>
2018-02-06 20:56:09 +0700

    first commit && page index

---
+++ index.php
@@ -0,0 +1,46 @@
+﻿<!DOCTYPE html>
+<html>
+<head >
+	<meta charset="UTF-8" />
+	<title>Welcome to Chat-RooM:.</title>
+ 
+<link rel="stylesheet" type="text/css" href="css/login.css" />
+
+</head>
+<body bgcolor = "#2E2E2E">
+<table  align = "center">
+		<br>
+		<br>
+	<tr><td>
+	<center><font color = "#DBDAE1" id="fff"> ยินดีต้อนรับสู่ห้องแชท</font></center><br>
+	</td>
+	</tr>
+	
+	<tr>
+	<td>
+		
+		<div>
+			<B>
+        <form method="post" action="c_login.php">
+    
+		<label for="username"><font color = "#E1E0E8">ชื่อผู้ใช้</font></label>
+		<input type="text" id="txtUsername" name="txtUsername">
+    
+		<label for="password"><font color = "#E1E0E8">รหัสผ่าน</font></label>
+		<input type="password" id="txtPassword" name="txtPassword">
+   
+		<input type="submit" name="Submit" value="เข้าสู่ระบบ">
+		<input type="reset" name="Reset" value="ยกเลิก"> 
+		
+		
+		</form>
+  </div>
+  </td>
+  </tr>
+	
+ </table>
+ </br>
+ <center><font color = "#DBDAE1" id="contact"> ***กรุณาติดต่อผู้ดูแลระบบ
+	เพื่อทำการขอชื่อผู้ใช้และรหัสผ่านที่ <br> www.facebook.com/boss.yotsapon และ <br>www.facebook.com/lamon.nwy </font></center><br>
+</body>
+</html>
